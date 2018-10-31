889cb9449563796b12f5ab28cd7e8653725746dd
Toby Kirk <toby.kirk@maths.ox.ac.uk>
2018-10-31 16:10:02 +0000

    Added more tests and a Dockerfile

--- test_heatequation.py
+++ test_heatequation.py
@@ -2,5 +2,11 @@ from heatequation import *
 import numpy as np
 
 def test_1():
-    y=heatequation(10,10,10,0.01,1,10)
-    assert (np.linalg.norm(y)/len(y)-10) < 10**(-2)
+    T0=10, Ta=10, Tb=10
+    T=heatequation(Ta,Tb,T0,0.01,1,100)
+    assert (np.linalg.norm(T)/len(T)-10) < 10**(-2)
+
+    T0=5, Ta=10, Tb=40
+    T=heatequation(Ta,Tb,T0,0.01,1,100)
+    assert max(T) <= max([T0,Ta,Tb])
+    assert min(T) >= min([T0,Ta,Tb])
